2c6c7f05abbc31454da04c9196b05ba1a6ef6495
rdwarakreddy <rdwarakreddy@gmail.com>
2026-09-12 02:08:42 +0530

    Add the development code

--- Terraform/modules/IRSA/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "app_irsa_role_arn" {
-  description = "ARN of the IAM role used by the Brew Minds application ServiceAccount. Put this in the ServiceAccount's eks.amazonaws.com/role-arn annotation in kubernetes/secrets.yml or the backend ServiceAccount manifest."
-  value       = aws_iam_role.app_irsa.arn
-}
-
-output "app_irsa_role_name" {
-  description = "Name of the application IRSA role."
-  value       = aws_iam_role.app_irsa.name
-}
